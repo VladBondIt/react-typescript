87c1fe3897b77a6fa1103d9bd3a9d97ad1d349ac
Vlad Bond <foolowin@gmail.com>
2021-01-18 18:12:49 +0300

    lesson 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,49 @@
+// import React from 'react';
+// import BigForm from './components/BigForm';
+// import MyComponent from './components/Portal';
+// import Context from './components/Portal';
+// import WithToggle from './components/HOC';
+// // import Counter from './components/Counter';
+// // import Form from './components/Form';
+
+// // Отличия от обычных функций, Реакт FC обеспечивают проверку типов и
+// // автозаполнения для статических свойст дисплей нейм, проп тайпс и дефолт пропс(есть проблемы с использованием).
+// // Так же Реакт FC предоставляют не явное определение потомков
+// // const Title: React.FC<{ title: string }> = ({ title, children }) => <h1>{title}{children}</h1>
+// // const Title: React.FC<{ title: string }> = ({ title }) => <h1>{title}</h1>
+
+// // type TitleProps = {
+// //     title: string,
+// // Если указать еще 1 параметр и не передать его в качестве пропса, то тоже будет ошибка на дев этапе,
+// // знак ? указывает не обязательность параметра.
+// //     test?: string
+// // }
+
+// // Указываем пропс по умолчанию
+// // const Title = ({ title, test = 'test2' }: TitleProps) => <h1>{title}</h1>
+
+// // Явная ошибка из за указания пропсов с помощью типа
+// // const App = () => <Title  />
+// // const App = () => <Title title="Test" />
+// // const App = () => <Counter title="Counter: " />
+// // const App = () => <Form />
+// // const App: React.FC = () => <WithToggle primTitle="Main Title" secTitle="Additional Title" />
+// const App: React.FC = () => <Context />
+
+// export default App;
+
+
 import React from 'react';
-import BigForm from './components/BigForm';
-import MyComponent from './components/Portal';
-import Context from './components/Portal';
-import WithToggle from './components/HOC';
-// import Counter from './components/Counter';
-// import Form from './components/Form';
 
-// Отличия от обычных функций, Реакт FC обеспечивают проверку типов и
-// автозаполнения для статических свойст дисплей нейм, проп тайпс и дефолт пропс(есть проблемы с использованием).
-// Так же Реакт FC предоставляют не явное определение потомков
-// const Title: React.FC<{ title: string }> = ({ title, children }) => <h1>{title}{children}</h1>
-// const Title: React.FC<{ title: string }> = ({ title }) => <h1>{title}</h1>
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
-// type TitleProps = {
-//     title: string,
-// Если указать еще 1 параметр и не передать его в качестве пропса, то тоже будет ошибка на дев этапе,
-// знак ? указывает не обязательность параметра.
-//     test?: string
-// }
-
-// Указываем пропс по умолчанию
-// const Title = ({ title, test = 'test2' }: TitleProps) => <h1>{title}</h1>
-
-// Явная ошибка из за указания пропсов с помощью типа
-// const App = () => <Title  />
-// const App = () => <Title title="Test" />
-// const App = () => <Counter title="Counter: " />
-// const App = () => <Form />
-const App: React.FC = () => <WithToggle primTitle="Main Title" secTitle="Additional Title" />
+const App: React.FC = ({ children }) => (
+    <>
+        <Header />
+        {children}
+        <Footer />
+    </>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,35 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+import App from './App';
+import Home from './components/home/home';
+import Contacts from './components/contacts/contacts';
+import Posts from './components/posts/posts';
+import Post from './components/post/post';
+
+import './style.css';
+
+ReactDOM.render((
+    <BrowserRouter>
+        <App>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/contacts' component={Contacts} />
+                <Route exact path='/posts' component={Posts} />
+                <Route path='/posts/:id' component={Post} />
+            </Switch>
+        </App>
+    </BrowserRouter>
+), document.getElementById('root'));
