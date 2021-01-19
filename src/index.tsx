bdd2560c277548c7877f236b62023a78e03a9c3c
Vlad Bond <foolowin@gmail.com>
2021-01-19 15:36:53 +0300

    start lesson 11

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import App from './App';
-// import Home from './components/home/home';
-// import Contacts from './components/contacts/contacts';
-// import Posts from './components/posts/posts';
-// import Post from './components/post/post';
-
-import './style.css';
 import { Provider } from 'react-redux';
 
-import store from './store'
+import App from './App';
+import store from './store';
+
+import './index.css';
 
 ReactDOM.render((
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 ), document.getElementById('root'));
-
-
-// ReactDOM.render((
-//     <BrowserRouter>
-//         <App>
-//             <Switch>
-//                 <Route exact path='/' component={Home} />
-//                 <Route path='/contacts' component={Contacts} />
-//                 <Route exact path='/posts' component={Posts} />
-//                 <Route path='/posts/:id' component={Post} />
-//             </Switch>
-//         </App>
-//     </BrowserRouter>
-// ), document.getElementById('root'));
