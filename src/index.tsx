2a06143bd6355f9c708c5f40cdccb10222f29abc
Vlad Bond <foolowin@gmail.com>
2021-01-19 15:32:34 +0300

    less11

--- src/index.tsx
+++ src/index.tsx
@@ -11,25 +11,35 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './App';
-import Home from './components/home/home';
-import Contacts from './components/contacts/contacts';
-import Posts from './components/posts/posts';
-import Post from './components/post/post';
+// import Home from './components/home/home';
+// import Contacts from './components/contacts/contacts';
+// import Posts from './components/posts/posts';
+// import Post from './components/post/post';
 
 import './style.css';
+import { Provider } from 'react-redux';
+
+import store from './store'
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route path='/contacts' component={Contacts} />
-                <Route exact path='/posts' component={Posts} />
-                <Route path='/posts/:id' component={Post} />
-            </Switch>
-        </App>
-    </BrowserRouter>
+    <Provider store={store}>
+        <App />
+    </Provider>
 ), document.getElementById('root'));
+
+
+// ReactDOM.render((
+//     <BrowserRouter>
+//         <App>
+//             <Switch>
+//                 <Route exact path='/' component={Home} />
+//                 <Route path='/contacts' component={Contacts} />
+//                 <Route exact path='/posts' component={Posts} />
+//                 <Route path='/posts/:id' component={Post} />
+//             </Switch>
+//         </App>
+//     </BrowserRouter>
+// ), document.getElementById('root'));
